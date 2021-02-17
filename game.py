ceb1a73de27b1ab7ce5db56325a1474a50a2c736
ash4317 <ashwinkulkarni82@gmail.com>
2021-02-17 16:27:43 +0530

    Made grid rows, cols global

--- game.py
+++ game.py
@@ -5,18 +5,23 @@ screen_info = pygame.display.Info()
 screen_width, screen_height = screen_info.current_w - 200, screen_info.current_h - 100
 screen = pygame.display.set_mode(size=(screen_width, screen_height))
 
+gridWidth = screen_width - 500
+gridRows, gridCols = 8, 8
+
 gray = (153, 153, 153)
+white = (255, 255, 255)
 run = True
 
 def getInitialPosition(rows, cols, height, width):
     """Sets the top-left corner position of the grid"""
     extraSpaceVertical, extraSpaceHorizontal = screen_height % rows, screen_width % cols
-    return int(extraSpaceVertical/2), int(extraSpaceHorizontal/2)
+    return (int(extraSpaceHorizontal/2), int(extraSpaceVertical/2))
 
 
 def drawGrid(rows, cols, height, width, color):
     """Draws the grid"""
-    top, left = getInitialPosition(rows, cols, height, width)   #get the top-left position of the grid
+    initialPosition = getInitialPosition(rows, cols, height, width) #get the top-left position of the grid
+    left, top = initialPosition[0], initialPosition[1]
 
     for i in range(rows):
         top = (i * height) + (1 * i)    #space of 1px between adjacent blocks
@@ -30,8 +35,8 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        h, w = int((screen_height - 7)/8), int((screen_width - 14)/15)
-        drawGrid(8, 15, h, w, gray)
+        height, width = int((screen_height - 7)/8), int((screen_width - 7 - 500)/8)
+        drawGrid(gridRows, gridCols, height, width, gray)
         pygame.display.update()
 
     pygame.quit()
